76e04f4c97228787ffacf1794c6417911214debb
Violeta Alanoca <v.isforvioleta@gmail.com>
2024-12-05 23:29:41 -0300

    Se cambia console log por table para orden + otros ejercicios

--- gestorInventario.js
+++ gestorInventario.js
@@ -16,7 +16,7 @@ const products = [
 // 1. Filtrar Productos con Descuento: Usa filter para obtener un nuevo array con los productos que tienen un descuento aplicado (es decir, discount > 0).
 
 const productosConDescuento = products.filter(conDescuento => conDescuento.discount > 0)
-console.log(productosConDescuento);
+console.table(productosConDescuento);
 
 
 // 2. Calcular el Precio Final con Descuento: Usa map para calcular el precio final de los productos que tienen descuento y crea un nuevo array que incluya el priceAfterDiscount.
@@ -32,9 +32,52 @@ const productosPrecioFinal = productosConDescuento.map((producto) => {
         priceAfterDiscount: priceAfterDiscount
     }
 })
-console.log(productosPrecioFinal);
+console.table(productosPrecioFinal);
 
 
 // 3. Identificar Productos con Stock Bajo: Usa un bucle para identificar los productos con menos de 5 unidades en inventario y guárdalos en un array nuevo.
+
+const productosConStockBajo = []
+for (const producto of products){
+    if(producto.stock < 5){
+        productosConStockBajo.push(producto)
+    }
+}
+console.table(productosConStockBajo);
+
+
 // 4. Actualizar el Stock de un Producto: Crea una función que reciba el nombre de un producto y una cantidad a agregar. Usa un try...catch para verificar si el producto existe en el array. Si existe, incrementa su stock; si no, lanza un error.
+
+function actualizarStockDeUnProducto(nombre, cantidadAAgregar) {
+  let productoEncontrado;
+  let productoExiste = false;
+
+  try {
+    for (const producto of products) {
+      if (producto.name == nombre) {
+        productoEncontrado = producto;
+        productoExiste = true;
+        break;
+      }
+    }
+
+    if (productoExiste) {
+      productoEncontrado.stock = productoEncontrado.stock + cantidadAAgregar
+      console.log(productoEncontrado)
+    } else {
+      throw new Error("Producto no encontrado")
+    }
+ 
+  }
+
+  catch (error) {
+    console.error(error.message);
+  }
+
+}
+
+
+actualizarStockDeUnProducto('Leche', 9);
+actualizarStockDeUnProducto('Mantequilla', 9);
+
 // 5. Resumen por Categorías: Usa un bucle para contar cuántos productos hay en cada categoría (electrónica, hogar, alimentos) y devuelve un objeto con este resumen.
